73225ed69e2e45d16bf84ab4b7da956e0f9ed3de
daviddinh1 <91696365+daviddinh1@users.noreply.github.com>
2024-05-09 18:16:36 -0500

    created the first section of the mock website need to fill in skew gaps and add elements to grid items

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,35 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true, // This ensures the dist folder is cleaned before each build
   },
   plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
-    ],
-  module: {
-  rules: [
-    {
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader'],
-    },
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
   ],
- },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[hash][ext][query]",
+        },
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
 };
